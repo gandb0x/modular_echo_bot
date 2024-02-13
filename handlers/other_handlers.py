ab99035507db49d4345c42a8dbdb38b8b5a4a17c
gandb0x <dop1gram@gmail.com>
2024-02-13 12:06:50 +0300

    Update other_handlers.py

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -5,7 +5,6 @@ from lexicon.lexicon import LEXICON_RU
 # Инициализируем роутер уровня модуля
 router = Router()
 
-
 # Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
 @router.message()
